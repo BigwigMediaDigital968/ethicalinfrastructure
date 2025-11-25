079a1bb97da60a2a7aafee9ac9e42924e177e843
unknown <chandanpms@gmail.com>
2025-11-25 12:01:52 +0530

    added redirection in next config

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,18 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["res.cloudinary.com", "www.google.com", "www.eiplin.com"],
   },
+
+  async redirects() {
+    return [
+      {
+        source:
+          "/blogs/luxury-living-in-gurgaon-top-residential-projects-recommended-by-ethical-infrastructures-private-limited",
+        destination:
+          "/blogs/real-estate-company-in-gurgaon-presenting-top-residential-projects",
+        permanent: true, // 301 SEO redirect
+      },
+    ];
+  },
 };
 
 export default nextConfig;
